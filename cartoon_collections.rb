b8c8151a35f1835f797c79d5782e190df29de9ee
Robin Kim <rkim1214@gmail.com>
2018-10-17 16:18:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include?(cheese_types)
+  if array.include?("cheddar", "gouda", "camembert")
     array.find { |i| i == "cheddar" || i == "gouda" || i == "camembert"}
   end 
 end
